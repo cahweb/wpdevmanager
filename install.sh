f65c4244986f3ca65c1fd9c06a463f1b903063f1
Ale Vecchi <ale@Ales-MacBook-Pro.local>
2017-01-25 18:30:09 -0500

    began working on pull function

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 
 mkdir -p ~/.config/ ~/.config/wpdevmanager
 
-echo -ne "\nwhat is the dev server's local ip address?: "
+echo -ne "what is the dev server's local ip address?: "
 read ip
 
 echo -n "which user would you like to ssh as?: "
@@ -15,14 +15,24 @@ read user
 echo -n "what is your servers http root directory?: "
 read srvroot
 
-echo -e "\n${YELLOW}please ensure to copy your ssh keys to the dev to simplify every transaction\n${NC}"
+echo -n "what is your local http root directory?: "
+read locroot
+
+echo -n "is your local machine using MAMP [y/N]: "
+read mac
+
+if [ "$mac" = "y" ]; then
+	echo -e "\ncreating symlink to /Applications/MAMP/Library/bin/mysql to /usr/local/bin"
+	ln -s /Applications/MAMP/Library/bin/mysql /usr/local/bin
+fi
+
+
+echo -e "${YELLOW}please ensure to copy your ssh keys to the dev to simplify every transaction${NC}"
 
 echo "installing..."
 install -m 775 bin/* /usr/local/bin
 
-echo -e "\ncreating config file ~/.config/wpdevmanager/config"
-echo -e "USER=$user\nIP=$ip\nSRVROOT=$srvroot" > ~/.config/wpdevmanager/config
+echo -e "creating config file ~/.config/wpdevmanager/config"
+echo -e "USER=$user\nIP=$ip\nSRVROOT=$srvroot\nLOCROOT=$locroot" > ~/.config/wpdevmanager/config
 
-echo -e "\nfixing permissions..."
-
-echo -e "\n${GREEN}done.${NC}\n"
+echo -e "${GREEN}done.${NC}"
